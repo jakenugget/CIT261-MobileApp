6d4d58960f585742e47ac3fc38bf94f915c5e196
jakenugget <59673380+jakenugget@users.noreply.github.com>
2020-02-14 22:03:17 -0700

    Update JSONobjects.js

--- JSONobjects.js
+++ JSONobjects.js
@@ -76,3 +76,16 @@ let alltimefive = JSON.stringify(startingfive);
 document.getElementById("tenthExample").innerHTML = JSON.stringify(
 startingfive,  
 );
+
+function updateTransition() {
+  var el = document.querySelector("div.box");
+
+  if (el) {
+    el.className = "box1";
+  } else {
+    el = document.querySelector("div.box1");
+    el.className = "box";
+  }
+
+  return el;
+}
