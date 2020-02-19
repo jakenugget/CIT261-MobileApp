7c5d3af9d34769b1c2ce37726ba2e53a9ed6c73a
jakenugget <59673380+jakenugget@users.noreply.github.com>
2020-02-18 22:38:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function updateTransition() {
 
   return el;
 }
+const myCanvas = document.getElementById('myCanvas');
+
+function canvas() {
+  var c = myCanvas.getContext('2d');
+  c.fillstyle = 'rgba(255, 25, 52, 0.8)';
+  
+  c.fillRect(20, 10, 80, 140);
+}
+
+function canvas1() {
+  var c = myCanvas.getContext('2d');
+  c.fillstyle = 'rgba(150, 99, 200, 0.5)';
+  c.beginPath();
+  c.arc(190,110,40,0,2*Math.PI);
+  c.stroke();
+  c.fill;
+}
